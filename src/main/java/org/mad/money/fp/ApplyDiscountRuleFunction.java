51a5cd19aa4609eb5c7f6794f2e6dbf50d6744f3
Xiaozhu Yang <xzyang@thoughtworks.com>
2020-05-20 15:36:33 +0800

    implemented ApplyDiscountRuleFunction

--- src/main/java/org/mad/money/fp/ApplyDiscountRuleFunction.java
+++ src/main/java/org/mad/money/fp/ApplyDiscountRuleFunction.java
@@ -1,4 +1,9 @@
 package org.mad.money.fp;
 
+import java.math.BigDecimal;
+
 public class ApplyDiscountRuleFunction {
+    public boolean ShouldApplyDiscount(BigDecimal thresholdPrice, BigDecimal amount) {
+        return amount.compareTo(thresholdPrice) > 0;
+    }
 }
